6555ee23455e65e04bdf16548bd7cddd4e266914
Nilesh <nileshvi9731@gmail.com>
2023-08-17 22:38:35 +0530

    added a comment

--- chatGPT/src/main/java/com/example/chatGPT/ChatGptApplication.java
+++ chatGPT/src/main/java/com/example/chatGPT/ChatGptApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatGptApplication {
-
+ //this is the main class which runs 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatGptApplication.class, args);
 	}
